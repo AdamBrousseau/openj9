86f47d58de3e019121d4226709bdc6b7aab4f6d7
Adam Brousseau <adam.brousseau@ca.ibm.com>
2025-02-20 14:24:28 -0500

    Add dry run param
    Signed-off-by: Adam Brousseau <adam.brousseau@ca.ibm.com>

--- buildenv/jenkins/jobs/infrastructure/artifactory_cleanup.groovy
+++ buildenv/jenkins/jobs/infrastructure/artifactory_cleanup.groovy
@@ -159,8 +159,12 @@ def cleanupTime(artifactory_server, artifactory_repo , artifactory_days_to_keep_
             }
             artifactFoldersUnique = artifactFolders.unique()
             artifactFoldersUnique.each() { uri ->
-                //httpRequest authentication: artifactoryCreds, httpMode: 'DELETE', consoleLogResponseBody: true, url: uri
-                echo "Delete: $uri"
+                if ("${params.DRY_RUN}" == "false") {
+                    //httpRequest authentication: artifactoryCreds, httpMode: 'DELETE', consoleLogResponseBody: true, url: uri
+                    echo "Delete: $uri"
+                } else {
+                    echo "Dry run delete: $uri"
+                }
             }
             echo 'Deleted all the old artifacts'
             currentBuild.description += "<br>Deleted ${artifacts_to_be_deleted.size()} artifacts"
