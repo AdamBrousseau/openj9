6f9411ac716a5a3b799b335c8f3d068e1336f205
Adam Brousseau <adam.brousseau@ca.ibm.com>
2025-02-21 13:28:00 -0500

    Put back delete calls in Count clean
    Signed-off-by: Adam Brousseau <adam.brousseau@ca.ibm.com>

--- buildenv/jenkins/jobs/infrastructure/artifactory_cleanup.groovy
+++ buildenv/jenkins/jobs/infrastructure/artifactory_cleanup.groovy
@@ -117,8 +117,7 @@ def cleanupBuilds(artifactory_server, artifactory_repo, jobToCheck, artifactory_
             for (i = 0; i < amount_deleted; i++) {
                 echo "Deleting Build #${folderNames[i]}"
                 if ("${params.DRY_RUN}" == "false") {
-                    //httpRequest authentication: artifactoryCreds, httpMode: 'DELETE', consoleLogResponseBody: true, url: "${artifactory_server}/${env.ARTIFACTORY_REPO}${internalSubfolder}${testSubfolder}/${jobToCheck}/${folderNames[i]}"
-                    echo "delete"
+                    httpRequest authentication: artifactoryCreds, httpMode: 'DELETE', consoleLogResponseBody: true, url: "${artifactory_server}/${env.ARTIFACTORY_REPO}${internalSubfolder}${testSubfolder}/${jobToCheck}/${folderNames[i]}"
                 }
             }
         } else {
@@ -128,8 +127,7 @@ def cleanupBuilds(artifactory_server, artifactory_repo, jobToCheck, artifactory_
         if (artifactory_max_num_artifacts == 0) {
             echo "Deleting Entire Build '${jobToCheck}'"
             if ("${params.DRY_RUN}" == "false") {
-                //httpRequest authentication: artifactoryCreds, httpMode: 'DELETE', consoleLogResponseBody: true, url: "${artifactory_server}/${env.ARTIFACTORY_REPO}${internalSubfolder}${testSubfolder}/${jobToCheck}"
-                echo "delete folder"
+                httpRequest authentication: artifactoryCreds, httpMode: 'DELETE', consoleLogResponseBody: true, url: "${artifactory_server}/${env.ARTIFACTORY_REPO}${internalSubfolder}${testSubfolder}/${jobToCheck}"
             }
         }
         currentBuild.description += "<br>Deleted ${amount_deleted} artifacts"
